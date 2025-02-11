d1a7b0b16a6f03f01c9ffdf05a3ff87fa196158c
Mateus <160790022+Mateusssssssssss@users.noreply.github.com>
2025-02-11 15:35:06 -0300

    apos tratamento . arquivo salvo

--- estudo.py
+++ estudo.py
@@ -5,13 +5,34 @@ import seaborn as sb
 dataset = pd.read_csv('case_estagio.csv')
 #verificar se a null em alguma coluna.
 null_data = dataset.isnull().sum()
-#indetificado que havia null na coluna Product_category_name, 
-#null bustituido por Outros
+# Descreve media. mediana. min e maximo, total de linhas.
+dataset_describe = dataset['price'].describe()
+print(dataset_describe)
+#Preenchendo valores nulos na coluna 'product_category_name' com 'Outros
 dataset.fillna('Outros', inplace=True)
 #agrupar por categoria
-agrupar = dataset.groupby(['product_category_name']).size()
-print(agrupar)
-#grafico de barras cor azul
-agrupar.plot.bar('blue')
-#Cria o grafico
+agrupar_category = dataset.groupby(['product_category_name']).size()
+#forma eficaz de visualizar a distribuição de um 
+# conjunto de dados e identificar outliers.
+#outliers indicam valores extremamente altos ou baixos 
+#em relação à distribuição dos dados.
+sb.boxplot(dataset['price']).set_title('Preços')
 plt.show()
+#Histograma dos preços para visualizar a distribuição dos valores e 
+#identificar possíveis valores exorbitantes.
+sb.displot(dataset['price'])
+plt.show()
+#soma de price por category
+agrupar_category_price = dataset.groupby(['product_category_name'])['price'].sum()
+#grafico de barras cor azul / Sales por categoria
+agrupar_category.plot.bar(color='blue')
+#Adiciona um titulo ao grafico
+plt.title('Número de Vendas por Categoria')
+#cria o grafico vendas por category
+plt.show()
+#Soma do preço de vendas por Category
+agrupar_category_price.plot.bar(color='red')
+#Cria o grafico soma de preço por category
+plt.title('Valores de vendas por Categoria')
+plt.show()
+dataset.to_csv('clean_case')
